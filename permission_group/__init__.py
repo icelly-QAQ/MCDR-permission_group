e17fd1e1d04c4036631308429d1729833a11d29e
icelly_QAQ <1787522500@qq.com>
2024-11-05 21:19:24 +0800

    add command

--- permission_group/__init__.py
+++ permission_group/__init__.py
@@ -1,15 +1,32 @@
-from typing import Literal
-from mcdreforged.api.command import *
-from mcdreforged.api.types import PluginServerInterface, Info
+from mcdreforged.api.all import *
 
-def all_command(server: PluginServerInterface):
-        server.register_command(
-        Literal("!!pg")
-        .then(Text("player"))
-        .then(Text("group"))
-    )
 
+help_msg = """§aPermission Group 帮助列表§r
+§b!!pg <player> <add|remove> <group> §f- §e把玩家添加或移除组"""
+
+
+def help_message(src: PluginServerInterface):
+    src.reply(help_msg)
+    
+def run_command(src: PluginServerInterface, context: CommandContext):
+    player = context["player"]
+    action = context["action"]
+    group = context["group"]
+    src.reply(f"player:{player},action:{action},group:{group}")
+
+def all_command(src: PluginServerInterface):
+    builder = SimpleCommandBuilder()
+
+    builder.command("!!pg", help_message)
+    builder.command("!!pg help", help_message)
+    builder.command("!!pg <player> <action> <group>", run_command)
+    builder.arg("player", Text)
+    builder.arg("action", Text)
+    builder.arg("group", Text)
+    builder.register(src)
+
+    
 def on_load(server: PluginServerInterface, old):
     server.logger.info('this plugin by icelly_QAQ!')
-    server.register_help_message('!!pg', "获取帮助")
+    server.register_help_message('!!pg help', "获取帮助")
     all_command(server)
