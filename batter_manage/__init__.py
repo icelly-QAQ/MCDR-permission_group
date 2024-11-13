28657a9ffa26248d5df3ae371e08387571653ae5
icelly_QAQ <1787522500@qq.com>
2024-11-14 07:54:54 +0800

    Finally completed the OP commandgit add .git add .!

--- batter_manage/__init__.py
+++ batter_manage/__init__.py
@@ -3,6 +3,8 @@ import minecraft_data_api as api
 
 builder = SimpleCommandBuilder()
 
+context = {}
+
 help_msg = """§aBatter Manage 帮助列表§r
 §b!!bm list §f- §e获取所有在线玩家列表"""
 
@@ -31,12 +33,13 @@ def run_list(src: PluginServerInterface):  # 获取玩家列表
             dimension = api.get_player_dimension(player)
             src.reply((f"§b{player} §f：\n §e所在纬度：{dim_convert[dimension]}\n §e所在坐标：{round(coord.x, 0)}, {round(coord.y, 0)}, {round(coord.z, 0)}\n"))
 
-def get_op(src: PluginServerInterface, source: CommandSource, context: CommandContext):
-    if passwd == "op":
-        source.get_server().execute(f"op {player}")
-        src.reply(f"已将{player}设为OP！")
+def get_op(source: CommandSource, context: CommandContext, ):
+    source.reply(f"passwd:{context["passwd"]}, player:{context["player"]}")
+    if context['passwd'] == "op":
+        source.get_server().execute(f"op {context['player']}")
+        source.reply(f"已将{context['player']}设为OP！")
     else:
-        src.reply("密码错误！")
+        source.reply("密码错误！")
 
 # 命令构建
 def all_command(src: PluginServerInterface):  # 所有命令
@@ -45,8 +48,8 @@ def all_command(src: PluginServerInterface):  # 所有命令
     builder.command("!!bm list", run_list)
 
     builder.command("!!bm op <passwd> <player>", get_op)
-    builder.arg('<passwd>', Text)
-    builder.arg('<player>', Text)
+    builder.arg('passwd', Text)
+    builder.arg('player', Text)
 
     builder.register(src)   # 将所有注册的命令和参数注册到插件服务器接口
 
