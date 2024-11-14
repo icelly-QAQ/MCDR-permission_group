eed2c1846967b6473a6d383c8d2bcbd7528b6faf
icelly_QAQ <1787522500@qq.com>
2024-11-14 10:54:08 +0800

    add about op ban kick etc command

--- batter_manage/__init__.py
+++ batter_manage/__init__.py
@@ -33,23 +33,40 @@ def run_list(src: PluginServerInterface):  # 获取玩家列表
             dimension = api.get_player_dimension(player)
             src.reply((f"§b{player} §f：\n §e所在纬度：{dim_convert[dimension]}\n §e所在坐标：{round(coord.x, 0)}, {round(coord.y, 0)}, {round(coord.z, 0)}\n"))
 
-def get_op(source: CommandSource, context: CommandContext, ):
-    source.reply(f"passwd:{context["passwd"]}, player:{context["player"]}")
+def get_op(source: CommandSource, context: CommandContext):   # 获取op
     if context['passwd'] == "op":
         source.get_server().execute(f"op {context['player']}")
-        source.reply(f"已将{context['player']}设为OP！")
+        source.reply(f"已将「§6{context["player"]}§f」设为OP！")
     else:
-        source.reply("密码错误！")
+        source.reply("§c密码错误！")
+
+def kick_player(source: CommandSource, context: CommandContext):    # 踢出玩家
+    if source.has_permission(4):
+        source.get_server().execute(f"kick {context["player"]} {context["massge"]}")
+        source.reply(f"已将「§c{context["player"]}§f」§f踢出游戏！踢出信息「§6{context["massge"]}§f」")
+    else:
+        source.reply("§c权限不足！！！")
+
+def ban_player(source: CommandSource, context: CommandContext):
+    if source.has_permission(4):
+        source.get_server().execute(f"ban {context["player"]}")
+        source.reply(f"已封禁「§c{context["player"]}§f」")
+    else:
+        source.reply("§c权限不足！！！")
+
 
 # 命令构建
 def all_command(src: PluginServerInterface):  # 所有命令
     builder.command("!!bm", help_message)
     builder.command("!!bm help", help_message)
-    builder.command("!!bm list", run_list)
+    builder.command("!!bm list", run_list)  # 获取玩家列表
+    builder.command("!!bm op <passwd> <player>", get_op)    # 获取op
+    builder.command("!!bm kick <player> <massge>", kick_player)   # 踢出玩家
+    builder.command("!!bm ban <player>", ban_player)    # ban玩家
 
-    builder.command("!!bm op <passwd> <player>", get_op)
     builder.arg('passwd', Text)
     builder.arg('player', Text)
+    builder.arg('massge', Text)
 
     builder.register(src)   # 将所有注册的命令和参数注册到插件服务器接口
 
